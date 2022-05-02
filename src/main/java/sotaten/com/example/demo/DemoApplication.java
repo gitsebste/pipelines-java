76dec597f05764f3535d941e7cef2de73574b9d5
user_name <porn@hub.gov>
2022-05-02 15:39:29 +0200

    test: changed port

--- src/main/java/sotaten/com/example/demo/DemoApplication.java
+++ src/main/java/sotaten/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package sotaten.com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,15 +7,14 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
 	@Scheduled(fixedRate = 1_000_000) // 16 min 40 sec
-	public void run(String... args) throws Exception {
+	public void scheduled() {
 
 	}
 }
