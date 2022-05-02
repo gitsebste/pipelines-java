78a8be3e72ca940a7ab5037c7ac82895f1859450
user_name <porn@hub.gov>
2022-05-02 15:22:52 +0200

    test: changed port

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM amazoncorretto:8-alpine-jdk
 COPY target/*.?ar app.jar
+EXPOSE 8180
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/sotaten/com/example/demo/DemoApplication.java
+++ src/main/java/sotaten/com/example/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package sotaten.com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableScheduling
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	@Scheduled(fixedRate = 1_000_000) // 16 min 40 sec
+	public void run(String... args) throws Exception {
+
+	}
 }
